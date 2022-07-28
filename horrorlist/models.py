654066603c996163e5c43455be765ff28870ec80
Timothy <tim.ttaj@gmail.com>
2022-07-27 17:15:36 -0700

    pagination done

--- horrorlist/models.py
+++ horrorlist/models.py
@@ -4,4 +4,5 @@ class ConsumedMovie(models.Model):
     title = models.CharField(verbose_name="movie title", max_length=200)
     date_added = models.DateTimeField(verbose_name="date consumed", auto_now=True)
     is_consumed = models.BooleanField(verbose_name="is consumed", default=False)
+    notes = models.TextField()
     
